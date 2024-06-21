6099065eae7183ff07e97903f332805d70b4f3e9
Aakash Satpute <aakash.satpute@esmagico.in>
2024-06-21 22:32:47 +0530

    Project setup

---
+++ test/logintest.spec.js
@@ -0,0 +1,92 @@
+const { remote } = require("webdriverio");
+
+let driver;
+
+// Load capabilities from environment variables
+const platformName = process.env.APPIUM_PLATFORM_NAME || "Android";
+const automationName = process.env.APPIUM_AUTOMATION_NAME || "UiAutomator2";
+const deviceName = process.env.APPIUM_DEVICE_NAME || "emulator-5554";
+const apkPath = process.env.APPIUM_APP_PATH || "App/app.apk";
+
+const wdOpts = {
+  hostname: process.env.APPIUM_HOST || "0.0.0.0",
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+  logLevel: "info",
+  capabilities: {
+    platformName,
+    "appium:automationName": automationName,
+    "appium:deviceName": deviceName,
+    "appium:app": apkPath,
+  },
+};
+
+describe("Mobile App Test", function () {
+  this.timeout(20000); // Set a longer timeout for Appium operations
+
+  before(async function () {
+    driver = await remote(wdOpts);
+  });
+
+  after(async function () {
+    if (driver) {
+      await driver.deleteSession();
+    }
+  });
+
+  it("login test", async () => {
+    if (!driver) {
+      throw new Error("Driver is not initialized. Test cannot be run.");
+    }
+
+    await driver.setTimeout({ implicit: 30000 });
+
+    try {
+      const privacyScrollerElement = await driver.$(
+        '//android.widget.ImageView[@resource-id="org.samagra.missionPrerna:id/privacy_scroller"]'
+      );
+      await privacyScrollerElement.click();
+      await driver.setTimeout({ implicit: 2000 });
+
+      const privacyCheckElement = await driver.$(
+        '//android.widget.CheckBox[@resource-id="org.samagra.missionPrerna:id/privacy_check"]'
+      );
+      await privacyCheckElement.click();
+      await driver.setTimeout({ implicit: 2000 });
+
+      const privacyAcceptElement = await driver.$(
+        '//android.widget.Button[@resource-id="org.samagra.missionPrerna:id/accept_policy"]'
+      );
+      await privacyAcceptElement.click();
+      await driver.setTimeout({ implicit: 5000 });
+      const notificationPermission = await driver.$(
+        '//android.widget.Button[@resource-id="com.android.permissioncontroller:id/permission_allow_button"]'
+      );
+      await notificationPermission.click();
+
+      await driver.setTimeout({ implicit: 5000 });
+      const noInputBox = await driver.$(
+        '//android.widget.EditText[@resource-id="org.samagra.missionPrerna:id/etResult"]'
+      );
+      await noInputBox.setValue("8668727053");
+
+      const sendOtpButton = await driver.$(
+        '//android.widget.Button[@resource-id="org.samagra.missionPrerna:id/btnCollect"]'
+      );
+      await sendOtpButton.click();
+      await driver.setTimeout({ implicit: 6000 });
+
+      // const otpElement =
+      //   '//*[@id="sourceContainer"]/div/div/div/div[3]/div/div/div/div[12]/span[3]';
+      // await otpElement.setValue("0000");
+      // //(//android.widget.FrameLayout[@resource-id="org.samagra.missionPrerna:id/otp_view_temp"])[1]c
+
+      // const submitOtp =
+      //   '//android.widget.Button[@resource-id="org.samagra.missionPrerna:id/validate_button"]';
+      // await submitOtp.click();
+    } finally {
+      if (driver) {
+        // await driver.deleteSession();
+      }
+    }
+  });
+});
